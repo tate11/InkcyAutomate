915d0137f40a2889ac7ac04c727c37eff3a03795
benninkcorien <benninkcorien@gmail.com>
2016-07-17 14:36:04 +0200

    Update automate.py
    Updated the To Do list at the bottom

--- automate.py
+++ automate.py
@@ -156,36 +156,122 @@ if __name__ == '__main__':
 # Make difference.csv look like import-products.csv
 # #id,    SKU,    Title,  Content,    Product Type,   parent_id   ,Product Categories ,Colors ,brand, ink attributes, paper,  nib size,   Image URL
 
-# -- id: blank
-
+# -- id: blank 
 # -- sku: blank (will be auto-generated on import)
+# -- Title : ink name (already working)
 
-# -- Title : ink name
+'''
 
-# -- Content: (if ink exists on fp-ink.info should have this format: )
+Needed: Find the ink on fp-ink.info, and pull information from the table for the matching row.
 
-# <a href="http://www.fp-ink.info/en/details/ .ink"><img src="http://www.fp-ink.info/colorcard/ .png"></a>
-# with the correct numbers for that ink on fp-ink.info (I have permission to use that website's images).
-# eg <a href="http://www.fp-ink.info/en/details/224.ink"><img src="http://www.fp-ink.info/colorcard224.png"></a>
+The table has the following header
+    Brand   Series  Name    Hue Lite    Vibrancy    L   D   Shade   Pig EG  LE  DC  Dry (s) Flow    Lubrication Bleed   Feather Ghost
+
+
+# Find match for ink. Either have python use the Search function and search on brandname first to get a smaller table
+# or just search through all 16 pages..
+
+# Once you've found a match, you need to access the correct row in the table
+
+# you can then get the values for that cell in that row using beautifulsoup :
+
+=== Beautifulsoup  Example on how to get data for all Columns from the table on the page (the_url) ===
+Needed: for specific row only
+
+START OF CODE 
+
+import urllib
+from bs4 import BeautifulSoup
+
+# the page that has the ink on it , the number at the end changes, currently goes up to 16.
+the_url = "https://www.fp-ink.info/en/?page=2" 
+
+r = urllib.urlopen(the_url).read().decode('utf-8')
+soup = BeautifulSoup(r, "html.parser")
+
+
+# get all Names :
+cols = [header.string for header in soup.find('thead').findAll('th')]
+namecolumn = cols.index('Name')
+name_values = [td[namecolumn].string.encode() for td in [tr.findAll('td') for tr in soup.find('tbody').findAll('tr')]]
+print name_values 
+
+# -- Brand
+# get all Brands :
+cols = [header.string for header in soup.find('thead').findAll('th')]
+brandcolumn = cols.index('Brand')
+brand_values = [td[brandcolumn].string.encode() for td in [tr.findAll('td') for tr in soup.find('tbody').findAll('tr')]]
+print brand_values 
+
+END OF CODE
+
+'''
+
+ 
+'''
+# Output of print cols[x] :
+
+# [None, u'Brand', u'Series', u'Name', None, None, None, None, u'D', None, None, None, None, None, None, None, None, None, None, None]
+
+# print cols[0] None - Details
+# print cols[1] Brand
+# print cols[2] Series
+# print cols[3] Name
+# print cols[4] None - Hue
+
+
+Needed - same process for Content and Color as we used for Names and Brands , 
+except that those have no header name, and cols.index[0] is not working...
+
+
+For Content: if the ink exists on fp-ink.info, the Content column in difference.csv should have this format: 
+ <a href="http://www.fp-ink.info/en/details/224.ink"><img src="http://www.fp-ink.info/colorcard224.png"></a>
+ Where the number 224 changes in two places
+
+ The individual ink links on fp-ink.info have the following format:
+ https://www.fp-ink.info/en/details/300.ink?page=13
+
+ You want to keep only the 300
+
+# STEP 1 
+# go to matching row, get the a href link of "Details"
+
+# I think something like this should work to get the href text so you can strip it and get the number.
+
+# details_url = table first column .find('link', href=True)
+# details_url_string = details_url.get('href')
+
+
+# strip the "https://www.fp-ink.info/en/details/" from the string, and then keep only the first numbers (until you hit the . in .ink)
+
+# STEP 2 store that value as 
+fpink_number =  # 300 in the example above
+
+# Generate the text for the Content column in difference.csv :
+
+content = '<a href="http://www.fp-ink.info/en/details/' + str(fpink_number) + '.ink"><img src="http://www.fp-ink.info/colorcard/' + str(fpink_number) + '.png"></a>'
+# print content  indeed gives the correct content, yay
+
+''' 
+
 
 # -- Product Type: simple
-
 # -- parent_id : 0
-
 # -- Product Categories: inks
 
-# -- Colors : the correct color for that ink
+'''
+Colors : the correct color for that ink
 # # Current colors used are black, blue, black, blue-black, brown, burgundy, gray, green, highlighter, white, invisible/white, orange, pink, purple, red, turquoise, yellow
 # # The fp-ink site sometimes has blue-green where I would have both blue and green
 
-# -- Brand
-# # Current brands used are Akkerman, Aurora, Blackstone, Caran d'Ache, Conway Stewart, Cross, De Atramentis, Diamine, Faber-Castell, Franklin Christoph, J Herbin, Kaweco, KWZ, Lamy, Levenger, Montblanc, Montegrappa, Monteverde, Noodler's, Organics Studio, Parker, Pelikan, Pilot, Platinum, Private Reserve, Rohrer & Klingner, Sailor, Sheaffer, Stipula, Toucan, Visconti, Waterman
+'''
+
+
 
 # -- ink attributes:  has to be set manually for each one.
-
 # -- Paper : Neenah NEUTECH PS
-
 # -- nib size: has to be set manually for each one.
+
 
 # -- image URL
 # # I think I can batch upload the images into Wordpress, the problem is Wordpress will auto rename images that have the same name
@@ -199,7 +285,6 @@ if __name__ == '__main__':
 
 # --?? Suggestions welcome for more improvements.
 
-# -- I would like to keep code very readable (so I understand what it does), so there is no need to optimize/minimize it into gobbledygook
-# -- human-readable names for all please ;)
+# -- I would like to keep code very readable (so I understand what it does)
  
 
